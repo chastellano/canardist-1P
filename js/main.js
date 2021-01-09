26d079e7cfd35eaa1ccdbaef6823c0d2cebc3c86
Chastellano <charles.a.castellano@gmail.com>
2021-01-08 23:40:15 -0500

    Functional one player

--- js/main.js
+++ js/main.js
@@ -60,12 +60,20 @@ let buttColors = ['btn-primary', 'btn-secondary', 'btn-success', 'btn-danger', '
 let identityCards = [];
 let teams = {};
 let missions = [];
-let users = [];
+let userDivArr = []
 let currentMission = 0;
 let checkedArr = [];
 let submissionArr = [];
-let playerNames = ['Tom', 'Dick', 'Harry', 'Philonia', 'Rosemary', 'Bilbert', 'LongNamePerson', 'Oakar', 'Simba', 'Jabushi']
+let playerNames = ['Mufasa', 'Simba', 'Rafiki', 'Nala', 'Scar', 'Timon', 'Pumbaa', 'Zazu', 'Ed', 'Shenzi']
+let scoreboard = {
+    red: 0,
+    black: 0
+}
+let numPlayers;
+let missionMax;
+let checkboxArr;
 
+const gameBody = document.getElementById('gameBody');
 const identityDeal = document.getElementById('identityDeal');
 const identityHeader = document.getElementById('identityHeader');
 const identityReveal = document.getElementById('identityReveal');
@@ -82,73 +90,11 @@ const proposeModalHeader = document.getElementById('proposeModalHeader');
 const executeButt = document.getElementById('execute');
 const redCard = document.getElementById('redCard');
 const blackCard = document.getElementById('blackCard');
+const startButtArr = [document.getElementById('missionStart1'), document.getElementById('missionStart2'), document.getElementById('missionStart3'), document.getElementById('missionStart4'), document.getElementById('missionStart5')]
 
-let numPlayers;
-
-function shuffle(num, arr) {
-    let i, j, x;
-    for (i = arr.length - 1; i; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = arr[i];
-        arr[i] = arr[j];
-        arr[j] = x;
-    }
-    let randCards = arr.slice(0, num);
-    return randCards;
-}
-
-function dealIdentity(team1, team2) {
-    let blackCards = shuffle(team1, black);
-    let redCards = shuffle(team2, red);
-    identityCards = blackCards.concat(redCards);
-    console.log(identityCards)
-    return identityCards;
-}
-
-function missionArr() {
-    missionReveal.style.display = "none";
-    missionDeal.innerHTML = '';
-    numPlayers = parseInt(playersInput.value);
-    switch (numPlayers) {
-        case 5:
-            missions[0] = 2;
-            missions[1] = 3;
-            missions[2] = 2;
-            missions[3] = 3;
-            missions[4] = 3;
-            break;
-        case 6:
-            missions[0] = 2;
-            missions[1] = 3;
-            missions[2] = 4;
-            missions[3] = 3;
-            missions[4] = 4;
-            break;
-        case 7:
-            missions[0] = 2;
-            missions[1] = 3;
-            missions[2] = 3;
-            missions[3] = 4;//***
-            missions[4] = 4;
-            break;
-        case 8:
-        case 9:
-        case 10:
-            missions[0] = 3;
-            missions[1] = 4;
-            missions[2] = 4;
-            missions[3] = 5;//***
-            missions[4] = 5;
-            break;
-        default:
-            return;
-    }
-    missionReveal.style.display = "block"
-    missionDeal.innerHTML = missions.join(', ');
-}
-
-function showMissionStart() {
-    missionStart1.style.display = 'block';
+function startGame() {
+    players();
+    missionArr();
 }
 
 function players() {
@@ -195,132 +141,193 @@ function players() {
 
     identityHeader.innerHTML = 'The identity cards are: ';
     dealCard(identityCards, 'identityDeal');
-    identityReveal.style.display = 'block';
     startHide.style.display = 'none';
-    reloadReveal.style.display = 'block';
-    showMissionStart();
-    missionStart1.addEventListener('click', genUserModal)
+    gameBody.style.display = 'block';
+    missionStart1.addEventListener('click', firstProposal);
 }
 
-function newMissionButt() {
-
+function firstProposal() {
+    genUserDivArr();
+    proposeModal();
 }
 
-function genUserModal() {
-    $('#proposeModal').modal('show')
-    userButts.innerHTML = '';
-    numPlayers = parseInt(playersInput.value);
-    proposeModalHeader.innerHTML = `Choose ${missions[currentMission]} people to go on your mission:`
+function genUserDivArr() {
     for (let i = 0; i < numPlayers; i++) {
         let div = document.createElement('div');
-        // div.className = 'userButtDiv';
         div.innerHTML = `
             <input type="checkbox" class="hideInput checkcheck" id="${playerNames[i]}" name="user${i+1}" value="${playerNames[i]}">
             <label class="btn ${buttColors[i]} btn-circle" for="${playerNames[i]}">${playerNames[i]}</label>
         `;
-        userButts.appendChild(div);
+        userDivArr.push(div);
     }
+}
 
-    let missionMax = parseInt(`${missions[currentMission]}`)
-    let checkboxArr = document.querySelectorAll('.checkcheck');
-    let checkedCount = 0
-    checkedArr = []
-    checkcheck();
-    function checkcheck() {
-        for (i = 0; i < checkboxArr.length; i++) {
-            checkboxArr[i].addEventListener('change', function() {
-                if (this.checked && (checkedArr.length < missionMax)) {
-                    checkedArr.push(this.nextElementSibling.innerHTML);
-                } else if (this.checked && (checkedArr.length === missionMax)) {
-                    this.checked = false;
-                    proposeModalHeader.innerHTML = `You can <strong class='text-danger'>ONLY</strong> choose ${missions[currentMission]} people:`;
-                } else if (!this.checked) {
-                    let index = checkedArr.indexOf(this.nextElementSibling.innerHTML)
-                    checkedArr.splice(index, 1)
-                }
-            });
-        }
+function missionArr() {
+    missionDeal.innerHTML = '';
+    // numPlayers = parseInt(playersInput.value);
+    switch (numPlayers) {
+        case 5:
+            missions[0] = 2;
+            missions[1] = 3;
+            missions[2] = 2;
+            missions[3] = 3;
+            missions[4] = 3;
+            break;
+        case 6:
+            missions[0] = 2;
+            missions[1] = 3;
+            missions[2] = 4;
+            missions[3] = 3;
+            missions[4] = 4;
+            break;
+        case 7:
+            missions[0] = 2;
+            missions[1] = 3;
+            missions[2] = 3;
+            missions[3] = 4;//***
+            missions[4] = 4;
+            break;
+        case 8:
+        case 9:
+        case 10:
+            missions[0] = 3;
+            missions[1] = 4;
+            missions[2] = 4;
+            missions[3] = 5;//***
+            missions[4] = 5;
+            break;
+        default:
+            return;
     }
+    missionDeal.innerHTML = missions.join(', ');
+}
+
+// const proposeTemplate = function() {
+    // for (let i=0; i < userDivArr.length; i++) {
+    //     userButts.appendChild(userDivArr[i]);
+    // }
+    // missionMax = parseInt(`${missions[currentMission]}`);
+    // console.log('test');
+    // checkcheck(checkboxArr, checkedArr, missionMax);
+// }
+
+// function checkcheck(arr, checked, max) {
+//     console.log(arr, checked, max);
+//     for (i = 0; i < arr.length; i++) {
+//         arr[i].addEventListener('click', function() {
+//             console.log('click');
+//             if (this.checked && (checked.length < max)) {
+//                 checked.push(this.nextElementSibling.innerHTML);
+//                 console.log(`Max: ${max}`);
+//             } else if (this.checked && (checked.length === max)) {
+//                 this.checked = false;
+//                 console.log(`Max: ${max}`);
+//                 proposeModalHeader.innerHTML = `You can <strong class='text-danger'>ONLY</strong> choose ${missions[currentMission]} people:`;
+//             } else if (!this.checked) {
+//                 let index = checked.indexOf(this.nextElementSibling.innerHTML)
+//                 checked.splice(index, 1);
+//                 console.log(`Max: ${max}`);
+//             }
+//         });
+//     }
+// }
+
+function proposeModal() {
+    $('#proposeModal').modal('show')
+    $('#userButts').empty();
+    checkedArr = [];
+    // checkboxArr = $('#userButts .checkcheck');
+    missionMax = parseInt(`${missions[currentMission]}`);
+    console.log(`Mission: ${currentMission}, Max: ${missionMax}`)
+    proposeModalHeader.innerHTML = `Choose ${missions[currentMission]} people to go on your mission:`
+    for (let i=0; i < userDivArr.length; i++) {
+        userButts.appendChild(userDivArr[i]);
+    }
+    checkboxArr = $('#userButts .checkcheck');
+    // missionMax = parseInt(`${missions[currentMission]}`);
+    checkboxArr.on('click', clickCheck);
+    function clickCheck() {
+        if (this.checked && (checkedArr.length < missionMax)) {
+            checkedArr.push(this.nextElementSibling.innerHTML);
+            console.log(`Checked: ${checkedArr.length}, Max: ${missionMax}`);
+        } else if (this.checked && (checkedArr.length === missionMax)) {
+            this.checked = false;
+            console.log(`Checked: ${checkedArr.length}, Max: ${missionMax}`);
+            proposeModalHeader.innerHTML = `You can <strong class='text-danger'>ONLY</strong> choose ${missions[currentMission]} people:`;
+        } else if (!this.checked) {
+            let index = checkedArr.indexOf(this.nextElementSibling.innerHTML)
+            checkedArr.splice(index, 1);
+            console.log(`Checked: ${checkedArr.length}, Max: ${missionMax}`);
+        }
+    };
+
+    $('#proposeEsc').click(function () {
+        $('#userButts').empty();
+        userDivArr = [];
+        console.log(userButts);
+        console.log(userDivArr);
+    });
     missionPropose.addEventListener('click', function() {
-        voteMission(checkedArr, missionMax);
+        console.log('Checks: ' + checkedArr.length + ', Max: ' + missionMax);
+        if (checkedArr.length === missionMax) {
+            voteMission(checkedArr, missionMax);
+            $('#proposeModal').modal('hide');
+            console.log(checkedArr, checkboxArr);
+            return;
+        } else {
+            document.getElementById('proposeModalHeader').innerHTML = `You <strong class='text-danger'>MUST</strong> choose ${missions[currentMission]} people:`;
+        }
     });
 }
 
-function startGame() {
-    players();
-    missionArr();
-}
-
 function voteMission(arr, max) {
-    if (arr.length === max) {
-        document.getElementById('proposedPlayers').innerHTML = ''
-        for (i=0; i < arr.length; i++) {
-            let div = document.createElement('div');
-            // div.className = 'userButtDiv';
-            div.innerHTML = `
-            <input type="checkbox" class="hideInput checkcheck" id="${arr[i]}">
-            <label class="btn ${buttColors[i]} btn-circle" for="${arr[i]}">${arr[i]}</label>
-            `;
-            document.getElementById('proposedPlayers').appendChild(div);
-        }
-        $(function() {
-            $('#proposeModal').modal('hide');
-            $('#voteModal').modal('show');
-        });
-        let vote;
-        let voteTotal = {
-            yea: 0,
-            nay: 0
-        };
-        // document.getElementById('thumbsUp').addEventListener('click', function() {
-        //     if(!this.checked) {
-        //         vote = 'yea';
-        //     }
-        // });
-        // document.getElementById('thumbsDown').addEventListener('click', function() {
-        //     if(!this.checked) {
-        //         vote = 'nay';
-        //     }
-        // });
-        voteSubmit.addEventListener('click', function() {
-            if ($("input[type=radio]:checked").length > 0) {
-                if ($("#yes").is(":checked")) {
-                    vote = 'yea'
-                    voteTotal.yea += 1;
-                } else if ($("#no").is(":checked")) {
-                    vote = 'nay'
-                    voteTotal.nay += 1;
-                }
-                dealMission(checkedArr)
-            } else {
-                $("#voteWarning").css("display","block");
-            }
-            // if(vote) {
-            //     if (vote='yea') {
-            //         voteTotal.yea += 1;
-            //     } else if (vote='nay') {
-            //         voteTotal.nay += 1;  
-            //     }
-            //     dealMission(checkedArr);
-            // } else {
-            //     document.getElementById('voteWarning').style.display = 'block';
-            // }
-        });
-    } else {
-        document.getElementById('proposeModalHeader').innerHTML = `You <strong class='text-danger'>MUST</strong> choose ${missions[currentMission]} people:`;
+    document.getElementById('proposedPlayers').innerHTML = ''
+    for (i=0; i < arr.length; i++) {
+        let div = document.createElement('div');
+        div.innerHTML = `
+        <input type="checkbox" class="hideInput checkcheck" id="${arr[i]}">
+        <label class="btn ${buttColors[i]} btn-circle" for="${arr[i]}">${arr[i]}</label>
+        `;
+        document.getElementById('proposedPlayers').appendChild(div);
     }
+    $('#voteModal').modal('show');
+    let vote;
+    let voteTotal = {
+        yea: 0,
+        nay: 0
+    };
+    
+    voteSubmit.addEventListener('click', function() {
+        if ($("#voteButts>div>input:checked").length > 0) {
+            if ($("#yes").is(":checked")) {
+                vote = 'yea'
+                voteTotal.yea += 1;
+
+            } else if ($("#no").is(":checked")) {
+                vote = 'nay'
+                voteTotal.nay += 1;
+            }
+            executeMission(checkedArr)
+            // console.log(checkedArr, checkboxArr)
+            return;
+        } else {
+            $("#voteWarning").css("display","block");
+        }
+        
+    });
 }
 
-function dealMission(arr) {
+function executeMission(arr) {
     let submission;
     let reds = shuffle(arr.length, red);
     let blacks = shuffle(arr.length, black);
     redCard.setAttribute('src', `cardsJS/cards/${reds[0]}.svg`);
     blackCard.setAttribute('src', `cardsJS/cards/${blacks[0]}.svg`);
+
     $('#voteModal').modal('hide');
     $('#missionModal').modal('show');
     executeButt.addEventListener('click', function() {
-        if ($("input[type=radio]:checked").length > 0) {
+        if ($("#passFailButts>div>input:checked").length > 0) {
             if ($("#pass").is(":checked")) {
                 submission = 'pass';
             } else if ($("#fail").is(":checked")) {
@@ -328,23 +335,79 @@ function dealMission(arr) {
             }
             $(missionModal).modal('hide');
             submissionArr.push(submission);
+            console.log(checkedArr);
             resolveMission();
+            return;
         } else {
             $("#executeWarning").css("display","block");
         }
     });
 }
 
-function resolveMission(arr) {
+function resolveMission() {
+    let currentMissionButt = startButtArr[currentMission];
+    missionStart1.removeEventListener('click', firstProposal);
     if (submissionArr.includes('fail')) {
-        console.log('Mission Fail!')
+        scoreboard.red += 1;
+        currentMissionButt.innerHTML = `Mission ${currentMission + 1} Failure!`;
+        currentMissionButt.classList.add('btn-danger');
+        currentMissionButt.classList.remove('btn-info');
     } else {
-        console.log('Mission Success!')
+        scoreboard.black += 1;
+        currentMissionButt.innerHTML = `Mission ${currentMission + 1} Success!`;
+        currentMissionButt.classList.add('btn-success');
+        currentMissionButt.classList.remove('btn-info');
     }
+    if ((scoreboard.red === 3) || (scoreboard.black === 3)) {
+        currentMissionButt.removeEventListener('click', proposeModal);
+        console.log('Game Over!');
+        if (scoreboard.red > 2) {
+            $('#winnerDiv h2').html('RED TEAM WINS!!!');
+        } else if (scoreboard.black > 2) {
+            $('#winnerDiv h2').html('BLACK TEAM WINS!!!');
+        }
+    } else {
+        let nextMissionButt = startButtArr[currentMission + 1];
+        nextMissionButt.addEventListener('click', proposeModal);
+        nextMissionButt.style.display = 'block';
+        currentMissionButt.removeEventListener('click', proposeModal);
+        currentMission += 1;
+    }
+    console.log(scoreboard);
+    console.log(checkedArr)
+    clearAllInput();
+    return;
+}
 
+function clearAllInput() {
+    $('input[type=checkbox]').prop('checked', false);
+    $('input[type=radio]').prop('checked', false);
+    $('#userButts').empty();
+    $("#voteWarning").css("display","none");
+    $("#executeWarning").css("display","none");
+    checkedArr = [];
+    submission = '';
+    submissionArr = [];
+}
 
+function shuffle(num, arr) {
+    let i, j, x;
+    for (i = arr.length - 1; i; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = arr[i];
+        arr[i] = arr[j];
+        arr[j] = x;
+    }
+    let randCards = arr.slice(0, num);
+    return randCards;
+}
 
-    submissionArr = []
+function dealIdentity(team1, team2) {
+    let blackCards = shuffle(team1, black);
+    let redCards = shuffle(team2, red);
+    identityCards = blackCards.concat(redCards);
+    // console.log(identityCards;)
+    return identityCards;
 }
 
 function dealCard (cardArr, elementId) {
